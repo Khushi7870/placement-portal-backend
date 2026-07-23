98792adea471e19e332ef56f4b1ad90d34145cfd
khushi kumari <khushikumarinwd7870@gmail.com>
2026-07-24 00:27:42 +0530

    Day 4 completed - User entity created

--- placementportal/src/main/java/com/khushi/placementportal/PlacementportalApplication.java
+++ placementportal/src/main/java/com/khushi/placementportal/PlacementportalApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PlacementportalApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PlacementportalApplication.class, args);
 	}
 
