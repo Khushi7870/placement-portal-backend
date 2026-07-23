b82f8fa1c5c221a83b3e05e44afdc6fdbaea7bbb
khushi kumari <khushikumarinwd7870@gmail.com>
2026-07-23 22:15:21 +0530

    Day 3 Spring Boot setup completed

---
+++ placementportal/src/main/java/com/khushi/placementportal/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.khushi.placementportal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Placement Portal is Running Successfully!";
+    }
+}
---
+++ placementportal/src/test/java/com/khushi/placementportal/PlacementportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khushi.placementportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
