c9d63e8123f64019b4dbeda70dae3cb2d6e3058d
Khushi Kumari <khushikumarinwd7870@gmail.com>
2026-07-24 16:09:40 +0530

    Created LoginRequest DTO

--- placementportal/src/main/java/com/khushi/placementportal/controller/UserController.java
+++ placementportal/src/main/java/com/khushi/placementportal/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.khushi.placementportal.controller;
 
+import com.khushi.placementportal.dto.LoginRequest;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.khushi.placementportal.entity.User;
+import com.khushi.placementportal.repository.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +14,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping
     public String getUsers() {
         return "User Controller Working";
     }
+
     @PostMapping("/register")
     public String registerUser() {
         return "User Registered";
     }
+
+    @PostMapping("/login")
+    public String loginUser(@RequestBody LoginRequest request) {
+
+        String email = request.getEmail();
+        String password = request.getPassword();
+
+        return "Login Attempted For : " + email;
+    }
+
+    @GetMapping("/test-user")
+    public String testUser() {
+
+        return userRepository
+                .findByEmail("avi@gmail.com")
+                .map(User::getName)
+                .orElse("User Not Found");
+    }
+
 }
