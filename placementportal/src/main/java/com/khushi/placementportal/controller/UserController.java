de24020e739a4d1bd1e4bc971c2ba03d4cc5f616
Khushi Kumari <khushikumarinwd7870@gmail.com>
2026-07-26 00:42:15 +0530

    Day 6 Login API completed

--- placementportal/src/main/java/com/khushi/placementportal/controller/UserController.java
+++ placementportal/src/main/java/com/khushi/placementportal/controller/UserController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.khushi.placementportal.entity.User;
 import com.khushi.placementportal.repository.UserRepository;
+import com.khushi.placementportal.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -16,6 +18,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping
     public String getUsers() {
@@ -30,10 +35,10 @@ public class UserController {
     @PostMapping("/login")
     public String loginUser(@RequestBody LoginRequest request) {
 
-        String email = request.getEmail();
-        String password = request.getPassword();
-
-        return "Login Attempted For : " + email;
+        return userService.loginUser(
+                request.getEmail(),
+                request.getPassword()
+        );
     }
 
     @GetMapping("/test-user")
@@ -44,5 +49,7 @@ public class UserController {
                 .map(User::getName)
                 .orElse("User Not Found");
     }
+
+
 
 }
--- placementportal/src/main/java/com/khushi/placementportal/service/UserService.java
+++ placementportal/src/main/java/com/khushi/placementportal/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,5 +19,20 @@ public class UserService {
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+    public String loginUser(String email, String password) {
+
+        Optional<User> user = userRepository.findByEmail(email);
+
+        if(user.isPresent()) {
+
+            User existingUser = user.get();
+
+            if(existingUser.getPassword().equals(password)) {
+                return "Login Successful";
+            }
+        }
+
+        return "Invalid Email or Password";
     }
 }
