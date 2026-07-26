3fd5f6c0a17b643fda3657344cce588979028525
Khushi Kumari <khushikumarinwd7870@gmail.com>
2026-07-26 14:27:55 +0530

    Day 8 - Created Result entity

---
+++ placementportal/src/main/java/com/khushi/placementportal/entity/Result.java
@@ -0,0 +1,23 @@
+package com.khushi.placementportal.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "results")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String studentName;
+
+    private Integer score;
+
+    private Integer totalQuestions;
+}
