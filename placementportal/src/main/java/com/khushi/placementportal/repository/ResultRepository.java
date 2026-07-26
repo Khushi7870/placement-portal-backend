37a8e96e436ebcd751bf8c41d1b6bfffaccc7c51
Khushi Kumari <khushikumarinwd7870@gmail.com>
2026-07-26 18:30:52 +0530

    Day 8 - Created ResultRepository

---
+++ placementportal/src/main/java/com/khushi/placementportal/repository/ResultRepository.java
@@ -0,0 +1,9 @@
+package com.khushi.placementportal.repository;
+
+import com.khushi.placementportal.entity.Result;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ResultRepository extends JpaRepository<Result, Long> {
+
+}
