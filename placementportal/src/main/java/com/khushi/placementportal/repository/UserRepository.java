2f425c706f666b4a2123f0f1b3ff3d9f9e7fad0c
Khushi Kumari <khushikumarinwd7870@gmail.com>
2026-07-26 18:35:49 +0530

    Day 8 - Updated UserRepository with role search

--- placementportal/src/main/java/com/khushi/placementportal/repository/UserRepository.java
+++ placementportal/src/main/java/com/khushi/placementportal/repository/UserRepository.java
@@ -3,9 +3,12 @@ package com.khushi.placementportal.repository;
 import com.khushi.placementportal.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+    List<User> findByRole(String role);
+
 }
