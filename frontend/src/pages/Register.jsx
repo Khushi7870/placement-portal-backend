d2ea5c1eec9bd8dd969e8fb931a64b741a4f4d7e
Khushi Kumari <khushikumarinwd7870@gmail.com>
2026-08-01 03:25:17 +0530

    Day 10 Completed - Registration API connected with MySQL

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,17 +1,45 @@
-import React from "react";
+import { useState } from "react";
+import API from "../services/api";
 
 function Register() {
-  return (
-    <div className="register-page">
-      <h1>Register</h1>
+    const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [role, setRole] = useState("");
 
-      <form>
+const handleRegister = async (e) => {
+  e.preventDefault();
+
+  try {
+    const response = await API.post("/users/register", {
+      name,
+      email,
+      password,
+      role
+    });
+
+    alert(response.data);
+
+  } catch (error) {
+    console.error(error);
+    alert("Registration Failed");
+  }
+};
+
+  return (
+    <div className="login-container">
+      <h2>Student Registration</h2>
+
+      <form onSubmit={handleRegister}>
+
         <div>
           <label>Name</label>
           <input
-            type="text"
-            placeholder="Enter your name"
-          />
+             type="text"
+             placeholder="Enter your name"
+             value={name}
+             onChange={(e) => setName(e.target.value)}
+             />
         </div>
 
         <div>
@@ -19,23 +47,42 @@ function Register() {
           <input
             type="email"
             placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
         <div>
           <label>Password</label>
           <input
-            type="password"
-            placeholder="Enter your password"
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+         onChange={(e) => setPassword(e.target.value)}
+         />
+        </div>
+
+        <div>
+          <label>Role</label>
+          <input
+          type="text"
+          placeholder="Enter role"
+          value={role}
+          onChange={(e) => setRole(e.target.value)}
           />
         </div>
 
-        <button type="submit">Register</button>
+        <button type="submit">
+          Register
+        </button>
+
       </form>
 
       <p>
-        Already have an account? <a href="/login">Login</a>
+        Already have an account?
+        <a href="/login"> Login</a>
       </p>
+
     </div>
   );
 }
