65ceadec037dfff8a8e29ab91a54562bc5e98b3e
Khushi Kumari <khushikumarinwd7870@gmail.com>
2026-09-14 01:16:06 +0530

    Add HomeController

---
+++ Backend/src/main/java/com/khushi/placementportal/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.khushi.placementportal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Placement Portal Backend Running Successfully!";
+    }
+}
