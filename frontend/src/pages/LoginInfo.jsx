27d8b3aa584465fe001232545951ea6d43192b36
Khushi Kumari <khushikumarinwd7870@gmail.com>
2026-09-16 00:00:54 +0530

    Fix login info page

--- frontend/src/pages/LoginInfo.jsx
+++ frontend/src/pages/LoginInfo.jsx
@@ -2,20 +2,18 @@ import React from "react";
 
 function LoginInfo() {
 
-   const email = localStorage.getItem("userEmail");
+  const email = localStorage.getItem("userEmail");
 
   return (
     <div style={{ padding: "40px", textAlign: "center" }}>
 
       <h1>Login Information</h1>
 
-      {user ? (
+      {email ? (
         <div>
-
           <p>
-            <strong>Email:</strong> {user.email}
+            <strong>Email:</strong> {email}
           </p>
-
         </div>
       ) : (
         <p>No user is currently logged in.</p>
